c667334a4fd5d31ea1ac457070d0063672afc0f1
Manavendra Sen <manavendra4288@gmail.com>
2022-10-12 00:06:30 +0530

    build: init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+/** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')(['ui']);
 
 module.exports = withTM({
   reactStrictMode: true,
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,13 @@
 import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+
+export const Button = ({ ...rest }) => {
+  return (
+    <button
+      {...rest}
+      type='button'
+      className='mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+    >
+      Default
+    </button>
+  );
 };
